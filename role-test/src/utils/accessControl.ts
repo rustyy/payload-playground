6314e5ffc3be1df749bbae195a1eeeb1d6a69307
Felix Hofmann <felix.hofmann@rustyy.de>
2022-08-17 17:04:17 +0200

    documentation

--- role-test/src/utils/accessControl.ts
+++ role-test/src/utils/accessControl.ts
@@ -1,14 +1,17 @@
 export const accessControl =
   (role: 'a' | 'b') =>
   ({ req: { user } }) => {
+    // no user, no access.
     if (!user) {
       return false;
     }
 
+    // Grant access for admin users.
     if (user.role === 'admin') {
       return true;
     }
 
+    // Scope collection to a specific role.
     if (user.role !== role) {
       return false;
     }
